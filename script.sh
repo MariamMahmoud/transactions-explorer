cc15986737666b32195b98229d2039420e9424c7
Mariam Mahmoud <mariammahmoud@glovoapp.com>
2023-02-20 14:35:46 +0100

    feat: dockerizing app

---
+++ script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+echo "Waiting for Postgres to start..."
+# until  psql --help  2>/dev/null; do
+#   sleep 1
+# done
+
+until PGPASSWORD=mysecretpassword psql -d "$POSTGRES_DB" -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+echo "Postgres is up and running!"
+
+echo "Creating user and database..."
+
+PGPASSWORD=$POSTGRES_PASSWORD psql -v ON_ERROR_STOP=1 -d "$POSTGRES_DB" -h "$POSTGRES_HOST" -U "$POSTGRES_USER"  <<-EOSQL
+    CREATE USER myAdmin WITH PASSWORD 'mysecretpassword';
+    CREATE DATABASE transactionsDB;
+    GRANT ALL PRIVILEGES ON DATABASE transactionsDB TO myAdmin;
+EOSQL
+
+echo "User and database created!"
+
+
+
+#Install dependencies
+npm install
+
+echo "Running migrations......."
+
+# Run database migrations
+# npm run db:migrate
+
+# Start the Node.js application
+npm start
