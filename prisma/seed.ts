5b8b97c2802b7e397e9680cc89a7663384ad2041
Mariam Mahmoud <mariammahmoud@glovoapp.com>
2023-02-27 09:16:16 +0100

    refactor: removing connect from seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,17 +9,8 @@ import transactions from './seeds/transactions.json'
 const prisma = new PrismaClient()
 const parsedTransactions: {  
     id: string;
-    account: {
-        connect: {
-            id: string
-        }
-    },
-
-    category: {
-        connect: {
-            id: string,
-        }
-    }
+    accountId: string;
+    categoryId: string;
     reference: string;
     amount: number;
     currency: string;
@@ -30,28 +21,21 @@ async function main ()  {
     await prisma.account.createMany({data: accounts});
     await prisma.category.createMany({data: categories});
 
+    // TODO: clean up
     transactions.forEach(transaction => {
         parsedTransactions.push(
             {
                 id: transaction.id,
-                account: {
-                    connect: {
-                        id: transaction.accountId,
-                    }
-                },
-                category: {
-                    connect: {
-                        id: transaction.categoryId,
-                    }
-                },
+                accountId: transaction.accountId,
+                categoryId: transaction.categoryId,
                 reference: transaction.reference,
                 amount: transaction.amount,
                 currency: transaction.currency,
                 date: new Date(Date.parse(transaction.date)),
             }
         )
-    })
-    Promise.all(parsedTransactions.map(parsedTransaction => prisma.transaction.create({data: parsedTransaction})))
+    });
+    await prisma.transaction.createMany({data: parsedTransactions});
 }
 
 main()
